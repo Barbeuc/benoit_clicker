ca621a92682765aeb0716cb12851f1230b164f46
abrisset <adrien.brisset3456@gmail.com>
2017-06-25 16:16:18 +0200

    feat(front) jukebox

--- clicker.js
+++ clicker.js
@@ -5,10 +5,26 @@ $( document ).ready(function() {
         getRandomBenoit().play();
         $('#benoit-count').text(parseInt($('#benoit-count').text())+1);
     });
+    // change the value of Audio
+    $('#selection').on('change', function() {
+        change($(this).val());
+    });
 
 });
 
 // Returns a random Benoit
 function getRandomBenoit() {
     return new Audio('sounds/benoit'+Math.trunc(Math.random() * 3 + 1)+'.mp3');
+}
+function change(sourceUrl) {
+    var audio = document.getElementById("player");
+    var source = document.getElementById("mp3_src");
+
+    audio.pause();
+
+    if (sourceUrl) {
+        source.src = sourceUrl;
+        audio.load();
+        audio.play();
+    }
 }
